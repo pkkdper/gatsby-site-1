b18b56de47a68ddc5f77b751b4454482368ee21a
oliwia <oliwia.giercza@gmail.com>
2023-03-15 21:49:16 +0100

    siteMetadata

--- Programming/gatsby/src/pages/index.js
+++ Programming/gatsby/src/pages/index.js
@@ -7,7 +7,7 @@ const IndexPage = () => {
   return (
     <>
       <Layout pageTitle="Home Page">
-        <p>I'm making this by following the Gatsby Tutorial.</p>
+        <p>I'm making this by following the Gatsby Tut.</p>
         <StaticImage
           alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
           src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
